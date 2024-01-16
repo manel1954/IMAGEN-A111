44f7dba4220ee97611b0a2205111eec5196c22c8
Manel <ea3eiz@ea3eiz.com>
2024-01-16 20:14:40 +0100

    1

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -28,7 +28,7 @@ sed -i "4c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh cerrar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_RADIO_ON.png" /home/pi/Abrir_Radio.desktop
 #sed -i "10c Name[es_ES]=Cerrar DMO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=ON" /home/pi/status.ini
@@ -42,7 +42,7 @@ xterm -geometry 85x13+1215+846 -bg blue -fg white -fa ‘verdana’ -fs 9x -T CO
 
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
 #sed -i "10c Name[es_ES]=Abrir DMO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
