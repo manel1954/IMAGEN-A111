1c7d57873a3ed7141a54129bf5df8ccaba37ecb6
Manel <ea3eiz@ea3eiz.com>
2024-01-19 03:20:25 +0100

    1

--- instalar_RustDesk.sh
+++ instalar_RustDesk.sh
@@ -8,28 +8,6 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-                        # Comprueba si Anydesk está instalado
-                        estado_anydesk=$(awk "NR==30" /home/pi/info.ini)
-                        if [ "$estado_anydesk" = 'RustDesk=ON' ];then
-                        echo "\v\v\v\v\v\v"
-                        echo "${ROJO}"
-                        echo "***********************************************************************"
-                        echo "***********************************************************************"
-                        echo "                        RustDesk YA ESTÁ INSTALADO                       "
-                        echo "                      NO PUEDES VOLVER A INSTALARLO                    "
-                        echo "***********************************************************************"
-                        echo "***********************************************************************"
-                        sleep 4
-                        else
-                        echo "\v\v\v\v\v\v"
-                        echo "${VERDE}"
-                        echo "***********************************************************************"
-                        echo "***********************************************************************"
-                        echo "                         INSTALANDO RustDesk                            "
-                        echo "***********************************************************************"
-                        echo "***********************************************************************"
-                        sleep 2  
-
                         cd /home/pi/Downloads
                         sudo apt install ./rustdesk-1.2.3-aarch64.deb
                         clear
@@ -42,4 +20,4 @@ MARRON="\33[38;5;138m"
                         echo "***********************************************************************"                      
                         sed -i "30c RustDesk=ON" /home/pi/info.ini                        
                         sleep 3
-                        fi
+                        
