99bfcfb9a6315cca4277089d471bbb6795f8429b
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-01-21 22:34:09 +0100

    Update ejecutar_svxlink.sh

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -32,7 +32,7 @@ xterm -geometry 85x13+1215+626 -bg black -fg white -fa ‘verdana’ -fs 9x -T C
 
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=ON/OFF" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
